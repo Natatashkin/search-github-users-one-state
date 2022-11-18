dd973c97bd3c51b7236ba370abab2d59c554fb54
natasha <cs070283sng@gmail.com>
2022-11-18 17:56:45 +0200

    change title tag

--- src/components/ErrorMessage/ErrorMessage.jsx
+++ src/components/ErrorMessage/ErrorMessage.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import styles from "./ErrorMessage.module.scss";
 
 const ErrorMessage = ({ message }) => {
-  return <h2 className={styles.errorTitle}>{message}</h2>;
+  return <h3 className={styles.errorTitle}>{message}</h3>;
 };
 
 export default ErrorMessage;
